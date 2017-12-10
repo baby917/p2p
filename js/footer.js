86abd208d16546e884f69d1be478d37308a0266b
honey917 <445085702@qq.com>
2017-12-10 22:37:07 +0800

    xx

---
+++ js/footer.js
@@ -0,0 +1,31 @@
+var foot='<footer class="el_footer row">\
+	<div class="container">\
+		<div class="col-sm-8 footer_left">\
+			<p>国内专业IT外包服务商</p>\
+			<p>版权所有： 2006-2015成都昂格网络科技有限公司</p>\
+			<p>地  址： 成都市高新区府城大道西段399号天府新谷6号楼620-625</p>\
+			<p>电  话： 028-86868686 / 400-808-6840  邮箱： angge@163.cn</p>\
+			<p><a href="#">蜀ICP备14030149号-1</a>  <a href="#">蜀公网安备510114990541</a></p>\
+		</div>\
+		<div class="col-sm-2 footer_mid">\
+			<p>官方微博：</p>\
+			<div class="row">\
+				<a href="#">\
+				<img src="img/tecent.png"/>\
+				</a>\
+				<a href="#">\
+					<img src="img/sina.png"/>\
+				</a>\
+			</div>							\
+		</div>\
+		<div class="col-sm-2 footer_right">\
+			<p>官方微信：</p>\
+			<div class="row">\
+				<a href="#">\
+					<img src="img/concat.jpg"/>\
+				</a>\
+			</div>\
+		</div>\
+	</div>\
+</footer>';
+document.write(foot);
---
+++ js/tpl_nav_personal.js
@@ -0,0 +1,30 @@
+var nav_personal='<nav class="el_nav navbar-default">\
+	<div class="container">\
+		<div class="navbar-header">\
+			<a class="navbar-brand nav_logo" href="#">\
+				<img alt="Brand" src="img/logo.png">\
+			</a>\
+		</div>\
+		<ul class="navbar-nav nav">\
+			<li>\
+				<a href="index.html">首页 <span class="sr-only">(current)</span></a>\
+			</li>\
+			<li>\
+				<a href="invest.html">我要投资</a>\
+			</li>\
+			<li>\
+				<a href="borrowMessage.html">我要借款</a>\
+			</li>\
+			<li class="active">\
+				<a href="personal.html">个人中心</a>\
+			</li>\
+			<li>\
+				<a href="#">新手指引</a>\
+			</li>\
+			<li>\
+				<a href="#">关于我们</a>\
+			</li>\
+		</ul>\
+	</div>\
+</nav>';
+document.write(nav_personal);
